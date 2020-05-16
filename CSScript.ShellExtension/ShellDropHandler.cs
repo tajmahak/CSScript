2285318e853d67eef725a2b678d793fec8a13ed1
tajmahak <tajmahal@bk.ru>
2020-05-16 12:27:52 +1000

    восстановление

--- CSScript.ShellExtension/ShellDropHandler.cs
+++ CSScript.ShellExtension/ShellDropHandler.cs
@@ -1,7 +1,7 @@
 ﻿using SharpShell.Attributes;
 using SharpShell.SharpDropHandler;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -20,24 +20,16 @@ namespace CSScript.ShellExtension
 
         protected override void Drop(DragEventArgs dragEventArgs)
         {
-            List<string> args = new List<string>();
-            args.Add(SelectedItemPath);
-            args.Add("/a");
-            args.AddRange(DragItems);
-
-            StringBuilder a = new StringBuilder();
-            for (int i = 0; i < args.Count; i++)
+            StringBuilder args = new StringBuilder();
+            foreach (string dragItem in DragItems)
             {
-                a.AppendLine(i + ") " + args[i]);
+                if (args.Length > 0)
+                {
+                    args.Append(" ");
+                }
+                args.Append($"\"{dragItem}\"");
             }
-            MessageBox.Show(
-                
-                a.ToString()
-                
-                );
-
-            Program.Main(args.ToArray());
-            //Process.Start(SelectedItemPath, args.ToString());
+            Process.Start(SelectedItemPath, args.ToString());
         }
     }
 }
